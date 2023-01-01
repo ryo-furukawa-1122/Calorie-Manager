e4cb6064a197790a07147593ac67772bb5762fa6
Ryo Furukawa <furu.4se@gmail.com>
2023-01-01 16:32:34 +0900

    small fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className='flex items-baseline justify-between py-2 px-3 top-0 shadow-sm sticky bg-slate-50 dark:bg-slate-900 bg-opacity-30 dark:bg-opacity-30 z-10'>
         <a href='/'>
-          <img className='w-[60px] h-auto' src='./calorie-manager-logo.svg' alt='logo' />
+          <img className='w-[70px] h-auto' src='./calorie-manager-logo.svg' alt='logo' />
         </a>
         <ThemeSwitch />
     </header>
