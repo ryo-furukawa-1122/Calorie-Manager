3514ab42ae61f2c764feba09bb9151561ec9e66b
Ryo Furukawa <furu.4se@gmail.com>
2023-01-01 10:57:30 +0900

    Updated set button

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -9,9 +9,9 @@ const Settings = () => {
         { value: 'down', label: '減量'}
     ]
 
-    let element = document.getElementById('mealset');
-    const mealset = element.value;
-    console.log(mealset);
+    onclick = (() => {
+        let mealset = document.getElementById('set').value;
+    })
 
     return (
         <div className='flex-auto p-2 z-1 relative bg-slate-50 dark:bg-slate-700 w-5/6 m-auto rounded mt-3 mb-3 shadow-md text-slate-700 dark:text-slate-200'>
@@ -22,7 +22,7 @@ const Settings = () => {
             {/* <CalorieCalc /> */}
             <label className='m-2'>
                 食事数：
-                <input className='mb-2' id='mealset' type={'text'}/>
+                <input className='mb-2 dark:text-slate-700' id='mealset' type={'text'}/>
             </label>
             <button id='set' className='rounded bg-sky-300 dark:bg-sky-600 px-4 py-2'>セット</button>
         </div>
