ab573eb668ed14da91b1370c80adc1b8653c0f1a
Ryo Furukawa <furu.4se@gmail.com>
2023-01-07 22:05:20 +0900

    Calculation was adopted in table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const getNumArray = (n) => Array.apply(null, new Array(n)).map(function(v,i){ return 1+i;});
 
 const Table = (props) => {
     const mealset = props.mealSetState[0];
 
-    const rows = [...getNumArray(mealset), 'Total']
+    const rows = getNumArray(mealset)
     const cols = ['Meal', 'Carbohydrate (g)', 'Protein (g)', 'Fat (g)']
+    
+    const [total, setTotal] = useState(0);
+
+    const totalCalc = () => {
+        var tableElem = document.getElementById('table');
+        var rowElems = tableElem.rows;
+        var t = 0;
+        var len = rowElems.length-1;
+        for (let i = 0; i < len; i++) {
+            t += parseInt(rowElems[i].cells[1].innerText);
+        }
+        console.log('calculated')
+        return(t);
+        // document.getElementsByClassName('total').innerText = t;
+    }
+
+    const reCalc = () => {
+        setTotal(totalCalc());
+        console.log(total);
+    }
+
     
     return (
         <div className='p-2 z-1 relative bg-slate-50 dark:bg-slate-700 w-5/6 mx-auto my-10 rounded shadow-md text-slate-700 dark:text-slate-200 text-center'>
@@ -18,7 +39,7 @@ const Table = (props) => {
                         ))}
                     </tr>
                 </thead>
-                <tbody>
+                <tbody id='table' onChange={reCalc}>
                     {rows.map((row) => (
                         <tr>
                             <th scope='cols'>{row}</th>
@@ -27,6 +48,12 @@ const Table = (props) => {
                             <td><input className='dark:bg-slate-500 w-full rounded' type={'number'} placeholder='' min={0}/></td>
                         </tr>
                     ))}
+                    <tr>
+                        <th>Total</th>
+                        <td>{total}</td>
+                        <td>{total}</td>
+                        <td>{total}</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
