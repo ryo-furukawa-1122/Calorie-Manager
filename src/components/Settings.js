fa9ec153264208168341cc66cf5607dc52fa359c
Ryo Furukawa <furu.4se@gmail.com>
2023-01-07 14:04:11 +0900

    Updated settings

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -15,22 +15,25 @@ const Settings = (props) => {
         { value: '1.2', label: 'Normal'},
         { value: '1.5', label: 'Active'}
     ]
-
+    
     const handleMealNumChange = (event) => {
         setMealSet(Number(event.target.value));
     }
 
-    // const base = Number(document.getElementById('base').value);
+    const [base, setBase] = useState(0);
+    const [purAmp, setPurAmp] = useState(1);
+    const [actAmp, setActAmp] = useState(1);
+    const [intake, setIntake] = useState(0);
+    
+    const handleBase = () => {
+        setBase(Number(document.getElementById('base').value));
+        console.log('clicked')
+    }
 
-    // const [purAmp, setPurAmp] = useState(1);
-    // const [actAmp, setActAmp] = useState(1);
-
-    // const Calc = (base, purAmp, actAmp) => {
-    //     const intake = base * purAmp * actAmp;
-    //     return (intake);
-    // }
-
-    const intake = 2000;
+    const handleIntake = () => {
+        setIntake(Number(base * purAmp * actAmp));
+        console.log('clicked')
+    }
 
     return (
         <div className='flex flex-col relative'>
@@ -44,13 +47,13 @@ const Settings = (props) => {
                 <Select className='dark:text-slate-700 m-2' options={activity} />
                 <label className='m-2'>
                     食事数：
-                    <input className='dark:bg-slate-500 w-10 mx-3' id='mealset' type={'number'} max='10' min='1' onChange={handleMealNumChange} />
+                    <input className='dark:bg-slate-500 w-10 mx-3' id='mealset' type={'number'} max='10' min='1' onClick={handleMealNumChange} />
                     回
                 </label>
-                {/* <button id='set' className='rounded bg-sky-300 dark:bg-sky-600 px-4 py-2 w-2/5 mx-auto' onClick={() => {Table.reMake();}}>セット</button> */}
+                <button id='set' className='rounded bg-sky-300 dark:bg-sky-600 px-4 py-2 w-2/5 mx-auto' onClick={() => {handleBase();handleIntake();}}>セット</button>
             </div>
             <div className='p-2 bg-slate-50 dark:bg-slate-700 w-5/6 mx-auto my-5 rounded shadow-md'>
-                1日の摂取量：<span className='mx-3' id='intake'>{intake}</span>kcal
+                1日の摂取量：<span className='mx-3'>{intake}</span>kcal
             </div>
 
         </div>
