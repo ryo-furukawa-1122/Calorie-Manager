ecb01afbcfac81a71783a4df2ca3c182aa3cbbb5
Ryo Furukawa <furu.4se@gmail.com>
2023-01-13 09:03:29 +0900

    Controled local strage

--- src/components/LangSwitch.js
+++ src/components/LangSwitch.js
@@ -2,22 +2,23 @@ import React from 'react'
 import Select from 'react-select'
 
 const LangSwitch = () => {
-    const languages = [
-        { value: 'ja', label: 'Ja' },
-        { value: 'en', label: 'En' }
-    ];
-    // const languages = ['ja', 'en'];
+    // const languages = [
+    //     { value: 'ja', label: 'Ja' },
+    //     { value: 'en', label: 'En' }
+    // ];
+    const languages = ['ja', 'en'];
 
     function updateLanguage(visiblelanguage) {
         const hiddenElements = document.getElementsByClassName('hidden');
         if (hiddenElements.length !== 0)
-            Array.from(hiddenElements).forEach((hiddenElement) => {
-                hiddenElement.classList.remove('hidden');
-            });
+        Array.from(hiddenElements).forEach((hiddenElement) => {
+            hiddenElement.classList.remove('hidden');
+        });
         const hideLanguages = languages.filter(
             (language) => language !== visiblelanguage
         );
         hideLanguages.forEach((language) => {
+            console.log(language);
             const hideElements = document.getElementsByClassName(language);
             if (hiddenElements.length !== 0)
                 Array.from(hideElements).forEach((hideElement) => {
@@ -29,14 +30,15 @@ const LangSwitch = () => {
     function onLanguageChange(event) {
         const language = event.target.value;
         updateLanguage(language);
+        // console.log('onLanguageChange');
         localStorage.setItem('language', language);
     }
     
     function setLanguageOnLoad() {
         const language = localStorage.getItem('language');
         const languageSelect = document.getElementById('language-select');
-        console.log(languageSelect);
         languageSelect.addEventListener('change', onLanguageChange);
+        // console.log(languageSelect);
         languageSelect.value = language ? language : languages[0];
         updateLanguage(languageSelect.value);
     }
@@ -50,11 +52,11 @@ const LangSwitch = () => {
 
     return (
         <div className='flex mx-auto'>
-            <Select id='language-select' name='Language' options={languages} placeholder='Language' defaultValue={{label:'Ja', value:'ja'}} onChange={setLanguageOnLoad} />
-            {/* <select name='Language' id='language-select'>
-                <option value={'ja'} selected>Ja</option>
+            {/* <Select id='language-select' name='Language' options={languages} placeholder='Language' defaultValue={{label:'Ja', value:'ja'}} onChange={setLanguageOnLoad} /> */}
+            <select name='Language' id='language-select' onChange={setLanguageOnLoad}>
+                <option value={'ja'}>Ja</option>
                 <option value={'en'}>En</option>
-            </select> */}
+            </select>
         </div>
     )
 }
