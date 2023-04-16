6882f020b0096b536f2306074da45b8784e681d9
Ryo Furukawa <furu.4se@gmail.com>
2023-04-16 21:06:11 +0900

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='w-full h-[100px] border-t border-solid border-slate-100 dark:border-slate-700 flex justify-center items-center'>
-        &copy; 2023 Calorie Manager
+        &copy; 2023 Furukawa
     </footer>
   )
 }
